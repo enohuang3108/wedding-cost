32f693fbb142d46cb460d23835ddd89634ce3252
eno.huang <enohuang3108@gmail.com>
2025-05-09 17:24:56 +0800

    build(eslint): add eslint and remove prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,83 +1,41 @@
-/**
- * This is intended to be a basic starting point for linting in your app.
- * It relies on recommended configs out of the box for simplicity, but you can
- * and should modify this configuration to best suit your team's needs.
- */
-
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-	root: true,
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-		ecmaFeatures: {
-			jsx: true,
-		},
-	},
-	env: {
-		browser: true,
-		commonjs: true,
-		es6: true,
-	},
-
-	// Base config
-	extends: ['eslint:recommended'],
-
-	overrides: [
-		// React
-		{
-			files: ['**/*.{js,jsx,ts,tsx}'],
-			plugins: ['react', 'jsx-a11y'],
-			extends: [
-				'plugin:react/recommended',
-				'plugin:react/jsx-runtime',
-				'plugin:react-hooks/recommended',
-				'plugin:jsx-a11y/recommended',
-			],
-			settings: {
-				react: {
-					version: 'detect',
-				},
-				formComponents: ['Form'],
-				linkComponents: [
-					{ name: 'Link', linkAttribute: 'to' },
-					{ name: 'NavLink', linkAttribute: 'to' },
-				],
-				'import/resolver': {
-					typescript: {},
-				},
-			},
-		},
-
-		// Typescript
-		{
-			files: ['**/*.{ts,tsx}'],
-			plugins: ['@typescript-eslint', 'import'],
-			parser: '@typescript-eslint/parser',
-			settings: {
-				'import/internal-regex': '^~/',
-				'import/resolver': {
-					node: {
-						extensions: ['.ts', '.tsx'],
-					},
-					typescript: {
-						alwaysTryTypes: true,
-					},
-				},
-			},
-			extends: [
-				'plugin:@typescript-eslint/recommended',
-				'plugin:import/recommended',
-				'plugin:import/typescript',
-			],
-		},
-
-		// Node
-		{
-			files: ['.eslintrc.cjs'],
-			env: {
-				node: true,
-			},
-		},
-	],
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:jsx-a11y/recommended',
+        '@remix-run/eslint-config',
+    ],
+    plugins: ['@typescript-eslint', 'react', 'react-hooks', 'jsx-a11y'],
+    rules: {
+        'max-len': ['warn', { code: 100 }],
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+        'no-multiple-empty-lines': ['error', {
+            max: 1,
+            maxEOF: 0,
+            maxBOF: 0
+        }],
+        'indent': ['error', 4, { SwitchCase: 1 }],
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,35 @@
-import { defineConfig } from 'vite';
 import {
-	vitePlugin as remix,
-	cloudflareDevProxyVitePlugin,
+    cloudflareDevProxyVitePlugin,
+    vitePlugin as remix,
 } from '@remix-run/dev';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { getLoadContext } from './load-context';
 
 export default defineConfig({
-	plugins: [
-		cloudflareDevProxyVitePlugin({
-			getLoadContext,
-		}),
-		remix({
-			future: {
-				v3_fetcherPersist: true,
-				v3_relativeSplatPath: true,
-				v3_throwAbortReason: true,
-			},
-		}),
-		tsconfigPaths(),
-	],
-	ssr: {
-		resolve: {
-			conditions: ['workerd', 'worker', 'browser'],
-		},
-	},
-	resolve: {
-		mainFields: ['browser', 'module', 'main'],
-	},
-	build: {
-		minify: true,
-	},
+    plugins: [
+
+        cloudflareDevProxyVitePlugin({
+            getLoadContext,
+        }),
+        remix({
+            future: {
+                v3_fetcherPersist: true,
+                v3_relativeSplatPath: true,
+                v3_throwAbortReason: true,
+            },
+        }),
+        tsconfigPaths(),
+    ],
+    ssr: {
+        resolve: {
+            conditions: ['workerd', 'worker', 'browser'],
+        },
+    },
+    resolve: {
+        mainFields: ['browser', 'module', 'main'],
+    },
+    build: {
+        minify: true,
+    },
 });
